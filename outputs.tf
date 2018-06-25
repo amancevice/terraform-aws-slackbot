8929585e2198d2a47153c925f16a9ac345a5576b
Alexander Mancevice <amancevice@cargometrics.com>
2018-06-25 11:08:01 -0400

    output resource IDs

--- outputs.tf
+++ outputs.tf
@@ -13,19 +13,14 @@ output "api_name" {
   value       = "${aws_api_gateway_rest_api.api.name}"
 }
 
-output "kms_key_id" {
-  description = "KMS Key ID."
-  value       = "${aws_kms_key.slackbot.key_id}"
+output "callback_resource_ids" {
+  description = "API Gateway Resource IDs for Slack callbacks."
+  value       = "${zipmap("${var.callback_ids}", "${aws_api_gateway_resource.callback.*.id}")}"
 }
 
-output "encrypted_slack_verification_token" {
-  description = "Encrypted Slack verification token"
-  value       = "${local.encrypted_slack_verification_token}"
-}
-
-output "events_request_url" {
-  description = "Events Request URL."
-  value       = "${aws_api_gateway_deployment.api.invoke_url}/${aws_api_gateway_resource.events.path_part}"
+output "callback_topics" {
+  description = "SNS topics for Slack callbacks."
+  value       = ["${aws_sns_topic.callback_ids.*.name}"]
 }
 
 output "callbacks_request_url" {
@@ -33,15 +28,32 @@ output "callbacks_request_url" {
   value        = "${aws_api_gateway_deployment.api.invoke_url}/${aws_api_gateway_resource.callbacks.path_part}"
 }
 
+output "event_resource_ids" {
+  description = "API Gateway Resource IDs for Slack events."
+  value       = "${zipmap("${var.event_types}", "${aws_api_gateway_resource.event.*.id}")}"
+}
+
+output "event_topics" {
+  description = "SNS topics for Slack events."
+  value       = ["${aws_sns_topic.event_types.*.name}"]
+}
+
+output "events_request_url" {
+  description = "Events Request URL."
+  value       = "${aws_api_gateway_deployment.api.invoke_url}/${aws_api_gateway_resource.events.path_part}"
+}
+
+output "kms_key_id" {
+  description = "KMS Key ID."
+  value       = "${aws_kms_key.slackbot.key_id}"
+}
+
+output "slash_commands_request_url" {
+  description = "Slash commands base URL."
+  value       = "${aws_api_gateway_deployment.api.invoke_url}/slash-commands"
+}
+
 output "slash_commands_resource_id" {
   description = "Slash Command resource ID."
   value       = "${aws_api_gateway_resource.slash_commands.id}"
-}
-
-output "sns_topics" {
-  description = "SNS topics."
-  value       = [
-    "${aws_sns_topic.callback_ids.*.name}",
-    "${aws_sns_topic.event_types.*.name}"
-  ]
 }
