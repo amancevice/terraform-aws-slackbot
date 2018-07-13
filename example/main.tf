a2e3d4dfc7c6a9d163ffb1aa830f95fa87a9ac7b
Alexander Mancevice <amancevice@cargometrics.com>
2018-07-13 14:28:40 -0400

    Update example to v1

--- example/main.tf
+++ example/main.tf
@@ -6,6 +6,7 @@ module "slackbot" {
   source                   = "amancevice/slackbot/aws"
   callback_ids             = ["my_callback_1"]
   event_types              = ["channel_rename"]
-  slack_verification_token = "${var.slack_verification_token}"
-  auto_encrypt_token       = true
+  slack_access_token       = "${var.slack_access_token}"
+  slack_bot_access_token   = "${var.slack_bot_access_token}"
+  slack_signing_secret     = "${var.slack_signing_secret}"
 }
--- example/variables.tf
+++ example/variables.tf
@@ -1,3 +1,11 @@
-variable "slack_verification_token" {
-  description = "Slack verification token."
+variable "slack_access_token" {
+  description = "Slack OAuth access token."
+}
+
+variable "slack_bot_access_token" {
+  description = "Slack OAuth bot access token."
+}
+
+variable "slack_signing_secret" {
+  description = "Slack signing secret."
 }
