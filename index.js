e15aaac6b1a4c9b2623b1378e405aa2ccb872205
Alexander Mancevice <amancevice@cargometrics.com>
2019-03-16 20:55:34 -0400

    slackend ~> 1.3.2

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
 const slackend = require('slackend/aws');
-const lambda = slackend();
-exports.handler       = lambda.handler;
-exports.postMessage   = lambda.postMessage;
-exports.postEphemeral = lambda.postEphemeral;
+module.exports = slackend();
