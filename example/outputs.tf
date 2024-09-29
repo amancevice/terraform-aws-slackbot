47eb9728adea42aa7079728bd5989f589f069771
Alexander Mancevice <alexander.mancevice@hey.com>
2024-09-29 13:35:47 -0400

    fix example output

--- example/outputs.tf
+++ example/outputs.tf
@@ -1,2 +1,2 @@
-output "install_uri" { value = "https://${var.domain}/install" }
+output "install_uri" { value = "https://slack.${var.domain}/install" }
 # output "openapi" { value = module.slackbot.openapi.json }
